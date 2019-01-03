0b11fd3120eba7eb06df4f249dc09848c8ac06af
Erik Daniel Rodriguez Aguilar <erik.rodriguez@laureate.net>
2019-01-03 09:28:37 -0600

    traduccion a Espanol desde lang packages

--- lang/lang.js
+++ lang/lang.js
@@ -46,38 +46,45 @@
             message : { text : "message", placeholder: "message", msg : "Please enter a message."}
 				};
         var myItemsES = {
-            m1Title : {text : "Latino Radio TX"},
-            m2Title : {text : "Online Magazine"},
-            m1Body : {text : "Advertise here in our online radio, and get excelents results for your business!" +
-                             "varied music, livestreaming in diferent schedules, music genres: " + 
-                             "country, reguee, clasics, modern, etc. You can get announce your business with us for very" +
-                             "low price and nice offerts"},
-            m2Body : {text : "In our magazine we ofert"},
-            WWA:{text: "Who we are?"},
-            mission:{text: "Mission"},
-            services:{text: "Services"},
-            ContactUs:{text: "Contact Us"},
+            m1Title : {text : "Radio Latino TX"},
+            m2Title : {text : "Revista Online"},
+            m1Body : {text : "Anunciate en nustra Radio Online y obten los mejores resultados en publicidad para tu Negocio! " +
+                             "música variada, programación en vivo en diferentes horarios, segmentos de música: country, reguee, " + 
+                             "música del recuerdo, moderna, etc. Puedes anunciar tu negocio con nosotros contamos con bajos costos y promociones. "},
+            m2Body : {text : "En nuestra revista te ofrecemos bla bla bla bla bla bla."},
+            WWA:{text: "¿Quienes somos?"},
+            mission:{text: "Misión"},
+            services:{text: "Servicios"},
+            ContactUs:{text: "Contáctanos"},
             titleContender:{text:"Online Radio - Magazine"},
-            titleServices:{text:"Our Services"},
+            titleServices:{text:"Nuestros Servicios"},
             titleMagazine:{text:"Latino Magazine"},
-            titleWWA:{text:"Who we are?"},
-            p1WWA:{text:"this is a test"},
-            p2WWA:{text:"this is a test 2"},
-            p3WWA:{text:"this is a test 3"},
-            CloseModal:{text:"close"},
-            titleMision:{text:"Mission"},
-            pMission:{text:"this is the mission"},
-            titleContact:{text:"Contact Us"},
-            sendMessageButton:{text:"Send"},
-            location:{text:"Location"},
-            location1:{text:"Latino Magazine address <br> 5869 street"},
-            location2:{text:"Social Media"},
+            titleWWA:{text:"¿Quienes Somos?"},
+            p1WWA:{text:"Le invitamos a disfrutar de esta edición de El Latino Magazine, ya que empezaremos a añadir un poco de contenido " +
+                        "aquí y allá para que usted disfrute, porfavor, haganos saber que opina, y si usted tiene una idea de la historia de alguien o siente que alguien merece un poco de atención, " +
+                        "póngase en contacto con cualquiera de nosotros. ¡Esperamos con interés escuchar de usted!"},
+            p2WWA:{text:"Nos gustaria tomar un poco de tiempo para agradecerle a nuestros clientes "+
+                        "por poner sus anuncios en nuestra revista estos cuatro años y esperamos muchos años mas. Gracias!"},
+            p3WWA:{text:"Como sabran, El Latino Magazine classifieds es una revista dedicada a mejorar e incrementar el volumen de ventas en su negocio, " +
+                        "através de nuestros servicios de comercialización, que son elementos que se complementan para el logro de sus objetivos a corto, mediano y largo plazo. " +
+                        "Hemos desarrollado una estrategia completa con nuestros elementos de mercadeo como la revista física, " +
+                        "página de Facebook y nuestro sitio web. si compra un anuncio, lo ponemos en todos elementos de mercadeo."},
+            CloseModal:{text:"Cerrar Ventana"},
+            titleMision:{text:"Mision"},
+            pMission:{text:"Proveer un servicio de alta calidad en nuestras emisora y revista, brindando una plataforma difusión para las pequeños y grandes empresas dentro y fuera de nuestras fronteras " +
+                           "Haciendo uso de tecnología de última generación en la industria, y a la vez asegurando la estabilidad de nuestras transmisiones y la difusión de nuestra revista " +
+                           "'El Latino Magazine' para la satisfacción de nuestros oyentes y clientes."},
+            titleContact:{text:"Contáctanos"},
+            sendMessageButton:{text:"Enviar"},
+            location:{text:"Ubicacion"},
+            location1:{text:"Dirección del Latino Magazine  <br> Calle, 5869 no se"},
+            location2:{text:"Redes Sociales!"},
             location3:{text:"<small>Copyright &copy; Latino Magazine 2018</small>"},
             contact_me : {text : "Contact me"},
-            name : { text : "Name", placeholder: "Name", msg : "Please enter your name."},
-            email : { text : "email", placeholder: "email", msg : "Please enter your email."},
-            phone : { text : "phone", placeholder: "phone", msg : "Please enter your phone number."},
-            message : { text : "message", placeholder: "message", msg : "Please enter a message."}
+            name : { text : "Nombre", placeholder: "Nombre", msg : "Favor ingresa tu nombre."},
+            email : { text : "email", placeholder: "email", msg : "Favor coloca un email."},
+            phone : { text : "Telefono", placeholder: "Telefono", msg : "Favor coloca tu numero telefonico."},
+            message : { text : "Mensaje", placeholder: "Mensaje", msg : "ingresa un mensaje."}
         };
     	//console.log(myItemsUS.nameContact.placeholder);
       function langChange() {

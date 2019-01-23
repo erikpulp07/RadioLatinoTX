fcc6827231682aa3f447b0b6fe4194377c6f61cd
Erik Daniel Rodriguez Aguilar <37194153+erikpulp07@users.noreply.github.com>
2019-01-22 21:55:01 -0600

    Se agrego plugin de facebook + chatango

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,7 +1,7 @@
 <?php
 // Check for empty fields
 
-require 'class.phpmailer.php';
+require 'mailer.php';
 
 
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
@@ -14,32 +14,13 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
+$subject = "Radio Latino TX";
+
+ if(send_mail("erikpulp07@gmail.com", $email, $subject, $message,$phone)){
+      echo json_encode("{'msg':'El correo ha sido enviado de forma exitosa'}");
+ }
+ else{ //echo "Algo salio mal";
+      echo json_encode("{'msg':'Algo ha salido mal'}");   
+} 
 
 
-$mail = new PHPMailer;
-
-$mail->IsSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';                 // Specify main and backup server
-$mail->Port = 587;                                    // Set the SMTP port
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'erikpulp07@gmail.com';                // SMTP username
-$mail->Password = 'Pulp@nocha07';                  // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-
-$mail->From = $email;
-$mail->FromName = $name;
-$mail->AddAddress('eguigurensmariell@gmail.com', 'Mariel Vallecillo');  // Add a recipient
-//$mail->AddAddress('ellen@example.com');               // Name is optional
-
-$mail->IsHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'RadioLatinoTX!!!!';
-$mail->Body    = $message;
-
-if(!$mail->Send()) {
-   echo 'Message could not be sent.';
-   echo 'Mailer Error: ' . $mail->ErrorInfo;
-   exit;
-}
-
-echo 'Message has been sent';

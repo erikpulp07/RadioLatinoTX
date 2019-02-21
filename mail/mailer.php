ba35cd54fb45a8085bba28e0283db2a26e3c7db4
Erik Daniel Rodriguez Aguilar <37194153+erikpulp07@users.noreply.github.com>
2019-02-20 23:48:10 -0600

    Prueba sin spam

--- mail/mailer.php
+++ mail/mailer.php
@@ -10,6 +10,7 @@ function send_mail($to,$from,$subject,$msg,$phone, $name){
     //   $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
 
         //Content
+        $boundary = str_replace(" ", "", date('l jS \of F Y h i s A'));
 
         $msg    ='
         <div style="text-align:left">
@@ -26,22 +27,32 @@ function send_mail($to,$from,$subject,$msg,$phone, $name){
        // 'Reply-To: '.$from."\r\n" .
        // 'X-Mailer: PHP/' . phpversion();
 
-       $headers = [
-        'From'                      => "$name <$from>",
-        'Sender'                    => $from,
-        'Return-Path'               => $from,
-        'MIME-Version'              => '1.0',
-        'Content-Type'              => 'text/html; charset=UTF-8; format=flowed; delsp=yes',
-        'Content-Transfer-Encoding' => '8Bit',
-        'X-Mailer'                  => 'Hugo - Zen',
-      ];
-      $mime_headers = [];
-      foreach ($headers as $key => $value) {
-        $mime_headers[] = "$key: $value";
-      }
-      $mail_headers = join("\n", $mime_headers);
+        $headersMail = '';
+        $headersMail .= 'From: ' . $name . "\r\n" . 'Reply-To: ' . $from . "\r\n";
+        $headersMail .= 'Return-Path: ' . $from . "\r\n";
+        $headersMail .= 'MIME-Version: 1.0' . "\r\n";
+        $headersMail .= "Content-Type: multipart/alternative; boundary = \"" . $boundary . "\"\r\n\r\n";
+        $headersMail .= '--' . $boundary . "\r\n";
+        $headersMail .= 'Content-Type: text/html; charset=ISO-8859-1' . "\r\n";
+        $headersMail .= 'Content-Transfer-Encoding: base64' . "\r\n\r\n";
+        $headersMail .= rtrim(chunk_split(base64_encode($msg)));
 
-      mail($to, $subject, $msg, $mail_headers); 
+    //    $headers = [
+    //     'From'                      => "$name <$from>",
+    //     'Sender'                    => $from,
+    //     'Return-Path'               => $from,
+    //     'MIME-Version'              => '1.0',
+    //     'Content-Type'              => 'text/html; charset=UTF-8; format=flowed; delsp=yes',
+    //     'Content-Transfer-Encoding' => '8Bit',
+    //     'X-Mailer'                  => 'Hugo - Zen',
+    //   ];
+    //   $mime_headers = [];
+    //   foreach ($headers as $key => $value) {
+    //     $mime_headers[] = "$key: $value";
+    //   }
+    //   $mail_headers = join("\n", $mime_headers);
+
+      mail($to, $subject, $msg, $headersMail); 
 
         return true;
     } catch (Exception $e) {
